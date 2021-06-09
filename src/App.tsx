02c4ecbbf82a7e5744f010b3604eb609c9f15605
kacper <kacykvaa@gmail.com>
2021-06-09 07:57:52 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Counter} from './features/counter/Counter';
+
+function App() {
+    return (
+        <>
+            <Counter/>
+        </>
+    );
+}
+
+export default App;
